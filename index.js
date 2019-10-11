2ae147b5a62473bf19aad4c8d09cc5341ca2e1f5
Vlad <loban1927@gmail.com>
2019-10-11 12:08:49 +0300

    proj init

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Test() {
+  return <h1>he</h1>;
+}
+
+ReactDOM.render(<Test />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve("index.js"),
+  output: {
+    path: path.resolve("build")
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve("index.tpl.html"),
+      inject: "body",
+      file: "index.html"
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve("public"),
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
